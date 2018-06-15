517be11a740996042210c3c2537b18404f73bbfc
Idziku <iddziku@gmail.com>
2018-06-15 21:58:42 +0200

    Menu+
    Markup : - [ ] title cool font
                   - [x] menu background
                   - [ ] settings
                   - [ ] credits
                   - [ ] game
                   - [ ] menu song
                   - [ ] intro
                   - [ ] rain sound effect

---
+++ lasthuman/Assets/PlayVid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MovieTexture movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+        movie.Play();
+        movie.loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
