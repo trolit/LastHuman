a5c927db6b8cb7e4900b1c3e1e9f4ca38c5b7f98
Idziku <iddziku@gmail.com>
2018-06-16 22:55:40 +0200

    Menu+

---
+++ lasthuman/Assets/OnMouseOverColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OnMouseOverColor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text t;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        t.color = Color.red;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        t.color = Color.white;
+    }
+}
