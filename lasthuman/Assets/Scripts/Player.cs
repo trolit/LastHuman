d0824b5e3f9d677e9208fc4754aa6f50c51b628f
Idziku <iddziku@gmail.com>
2018-06-30 09:48:14 +0200

    Jump Animation+++

--- lasthuman/Assets/Scripts/Player.cs
+++ lasthuman/Assets/Scripts/Player.cs
@@ -13,6 +13,24 @@ public class Player : MonoBehaviour {
     private bool facingRight;
     private bool attack;
 
+    [SerializeField]
+    private Transform[] groundpoints;
+
+    [SerializeField]
+    private float groundRadius;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    private bool isGrounded;
+    private bool Jump;
+    [SerializeField]
+    private bool aircontrol;
+
+    [SerializeField]
+    private float JumpForce;
+
+
     // Use this for initialization
     void Start () {
         facingRight = true;
@@ -27,16 +45,28 @@ public class Player : MonoBehaviour {
 
 	void FixedUpdate () {
         float horizontal = Input.GetAxis("Horizontal");
+        isGrounded = isgrounded();
 
         HandleMovement(horizontal);
         Flip(horizontal);
         HandleAttacks();
+        HandleLayers();
         ResetValues();
 	}
 
     private void HandleMovement(float horizontal)
     {
-        if (!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        if (myrigidBody.velocity.y < 0 )
+        {
+            myAnimator.SetBool("land", true);
+        }
+        if (isGrounded && Jump == true)
+        {
+            isGrounded = false;
+            myrigidBody.AddForce(new Vector2(0, JumpForce));
+            myAnimator.SetTrigger("jump");
+        }
+        if (!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack") && (isGrounded || aircontrol))
         {
             myrigidBody.velocity = new Vector2(horizontal * movementSpeed, myrigidBody.velocity.y);
         }
@@ -59,6 +89,10 @@ public class Player : MonoBehaviour {
         {
             attack = true;
         }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump = true;
+        }
     }
 
     private void Flip(float horizontal)
@@ -76,5 +110,40 @@ public class Player : MonoBehaviour {
     private void ResetValues()
     {
         attack = false;
+        Jump = false;
+    }
+
+    private bool isgrounded()
+    {
+        if (myrigidBody.velocity.y <= 0)
+        {
+            foreach (Transform point in groundpoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if (colliders[i].gameObject != gameObject)
+                    {
+                        myAnimator.ResetTrigger("jump");
+                        myAnimator.SetBool("land", false);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void HandleLayers()
+    {
+        if (!isGrounded)
+        {
+            myAnimator.SetLayerWeight(1, 1);
+        }
+        else
+        {
+            myAnimator.SetLayerWeight(1, 0);
+        }
     }
 }
