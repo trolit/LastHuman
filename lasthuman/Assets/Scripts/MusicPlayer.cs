ea583a5784c5d402f4573fa0ee1c662ebe82dd55
Idziku <iddziku@gmail.com>
2018-07-01 21:29:33 +0200

    Menu Improvements

---
+++ lasthuman/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    public AudioClip[] clips;
+    private AudioSource audiosource;
+
+	// Use this for initialization
+	void Start () {
+        audiosource = FindObjectOfType<AudioSource>();
+        audiosource.loop = false;
+	}
+	
+    private AudioClip GetRandomClip()
+    {
+        return clips[Random.Range(0, clips.Length)];
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if(!audiosource.isPlaying)
+        {
+            audiosource.clip = GetRandomClip();
+            audiosource.Play();
+
+        }
+	}
+}
