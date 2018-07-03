0a20be99ff01ee8d2c184400eac00dddf6fafd37
Idziku <iddziku@gmail.com>
2018-07-03 10:50:55 +0200

    Movie optimalization

---
+++ lasthuman/Assets/Scripts/StreamVideo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+public class StreamVideo : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    void FixedUpdate()
+    {
+        while (!videoPlayer.isPlaying)
+        {
+            videoPlayer.Play();
+        }
+    }
+
+    IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while (!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+    }
+}
