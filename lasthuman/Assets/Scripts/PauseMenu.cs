7e0c83b29d28ec1a0eaeb0ed4bdefab970ce0b87
Idziku <iddziku@gmail.com>
2018-07-08 20:54:26 +0200

    Added
    - escape menu
    - SAVED text notification

---
+++ lasthuman/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool gameisPaused = false;
+    //static to check from other scripts whether key is pressed
+
+    public GameObject PauseMenuUI;
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameisPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameisPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameisPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ lasthuman/Assets/Scripts/ShowText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowText : MonoBehaviour
+{
+    public Text displayText;
+
+    public void DisplayText()
+    {
+        displayText.text = "SETTINGS SAVED!";
+        Invoke("HideText", 1.5f);
+    }
+
+    public void HideText()
+    {
+        displayText.text = "";
+    }
+}
