cd697f24120a5bdc0af6b7b2dde69be43d668a1f
Idzik <iddziku@gmail.com>
2018-07-13 11:33:55 +0200

    AI part2
    Interface: IEnemyState
    
    States for enemy:
    - attack
    - patrol
    - idle

---
+++ lasthuman/Assets/Scripts/EnemyStates/IdleState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : IEnemyState
+{
+    public void Enter(Enemy enemy)
+    {
+
+    }
+
+    public void Execute()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider2D other)
+    {
+
+    }
+}
